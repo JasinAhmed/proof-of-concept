0c9aec50d6b7f61ef7b1183b5da3e1980e089041
JasinAhmed <jaasin9@gmail.com>
2026-06-06 18:53:38 +0200

    #6 GET-routes toegevoegd voor huizenoverzicht en woningdetailpagina

--- server.js
+++ server.js
@@ -1,44 +1,51 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express';
+import { Liquid } from 'liquidjs';
 
-import express from 'express'
-import { Liquid } from 'liquidjs'
+const app = express();
 
-const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
+const engine = new Liquid();
 
-const engine = new Liquid()
-app.engine('liquid', engine.express())
+app.engine('liquid', engine.express());
+app.set('views', './views');
+app.set('view engine', 'liquid');
 
-app.set('views', './views')
-app.set('view engine', 'liquid')
+/* OVERZICHTSPAGINA */
 
-
-
-app.get('/huis-detail', async function (request, response) {
+app.get('/', async function (request, response) {
     const apiResponse = await fetch(
         'https://fdnd-agency.directus.app/items/f_houses?fields=*.*'
-    )
+    );
 
-    const apiResponseJSON = await apiResponse.json()
+    const apiResponseJSON = await apiResponse.json();
 
-    const houses = apiResponseJSON.data
+    response.render('index.liquid', {
+        title: 'Huizen',
+        houses: apiResponseJSON.data
+    });
+});
 
-    const house = houses[0]
+/* DETAILPAGINA */
 
-    console.log(Object.keys(house))
-    console.log(house)
+app.get('/huis-detail/:id', async function (request, response) {
+    const id = request.params.id;
+
+    const apiResponse = await fetch(
+        `https://fdnd-agency.directus.app/items/f_houses/${id}?fields=*.*`
+    );
+
+    const apiResponseJSON = await apiResponse.json();
 
     response.render('huis-detail.liquid', {
         title: 'Huis detail',
-        house: house
-    })
-})
+        house: apiResponseJSON.data
+    });
+});
 
-
-app.set('port', process.env.PORT || 8000)
+app.set('port', process.env.PORT || 8000);
 
 app.listen(app.get('port'), function () {
-    console.log(`Server draait op http://localhost:${app.get('port')}`)
-})
+    console.log(`Server draait op http://localhost:${app.get('port')}`);
+});
